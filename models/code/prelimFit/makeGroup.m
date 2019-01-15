4d98a7e7b06ea4879d50da1a0ce80179a0f0d7a3
Jason <jasonz1@student.unimelb.edu.au>
2019-01-15 15:59:04 +1100

    Just messing around with group

---
+++ models/code/prelimFit/makeGroup.m
@@ -0,0 +1,13 @@
+function [ Data1, Data2 ] = makeGroup(Alldata, size1, size2)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+ntrials1 = size1;  % (not anymore) Different numbers for Short and Long!
+ntrials2 = size2;
+Data1 = zeros(ntrials1, 6);
+Data2 = zeros(ntrials2, 6);
+
+
+
+end
+
