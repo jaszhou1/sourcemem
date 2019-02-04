a7278c867758a84dcd7b6d405a7444fa4285d132
Jason <jasonz1@student.unimelb.edu.au>
2019-02-04 13:18:59 +1100

    Plots
    Plots for improved fits for mix and VP

--- models/code/criterionVar/Criterionx/mixPredExcel.m
+++ models/code/criterionVar/Criterionx/mixPredExcel.m
@@ -1,6 +1,29 @@
+Fit_Data = Treatment_Simple;
+
+Recognised = Fit_Data(:,1);
+Unrecognised = Fit_Data(:,2);
+
 participants = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20];
 
 for i = participants
-preds(i,:)=VP_LL_Preds_Recognised{i,4};
+VPpreds(i,:)=VP_LL_Preds_Recognised{i,4};
+Mixpreds(i,:)=MX_LL_Preds_Recognised{i,4};
 end
 
+for i = participants
+    filename = ['VP_Recog',num2str(i),'.png'];
+    fitplot(Recognised {i}, VP_LL_Preds_Recognised{i,3});
+    saveas(gcf,filename);
+%     
+%     filename = ['1VP_Unrecog',num2str(i),'.png'];
+%     fitplot(Unrecognised {i}, VP_LL_Preds_Unrecognised{i,3});
+%     saveas(gcf,filename);
+    
+    filename = ['MX_Recog',num2str(i),'.png'];
+    fitplot(Recognised {i}, MX_LL_Preds_Recognised{i,3});
+    saveas(gcf,filename);
+    
+%     filename = ['2MX_Unrecog',num2str(i),'.png'];
+%     fitplot(Unrecognised {i}, MX_LL_Preds_Unrecognised{i,3});
+%     saveas(gcf,filename);
+end    
