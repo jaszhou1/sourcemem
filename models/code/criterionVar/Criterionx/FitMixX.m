22b32a6beb31a3dcc4bef486a2ef313c4f1d20bb
Jason Zhou <jasonz1@student.unimelb.edu.au>
2019-02-04 12:51:24 +1100

    Fits ver 3

--- models/code/criterionVar/Criterionx/FitMixX.m
+++ models/code/criterionVar/Criterionx/FitMixX.m
@@ -4,18 +4,18 @@ function [ll, bic, Pred, pest] = FitMixX(data)
 %          1    2    3    4    5      6    7   8   9   10    11
 setopt;
 
-v1a = normrnd(0.2,0.1);
-v2a = normrnd(0.01,0.05);
-v1b = normrnd(0.4,1);
-v2b = normrnd(0.01,0.05);
+v1a = normrnd(1,0.1);
+v2a = normrnd(0.01,0.5);
+v1b = normrnd(1,1);
+v2b = normrnd(0.01,0.5);
 eta1 = normrnd(0, 0.01);
 eta2 = normrnd(0,0.01);
-a1 = normrnd(0.5,0.5);
-a2 = normrnd(1.4,0.5);
-pi1 = norm(0.7,0.25);
-pi2 = normrnd(0.7,0.25);
+a1 = normrnd(1,0.5);
+a2 = normrnd(1,0.5);
+pi1 = norm(0.5,0.25);
+pi2 = normrnd(0.5,0.25);
 Ter = normrnd(0.2,0.1);
-sa = normrnd(0.09,0.05);
+sa = normrnd(1,0.5);
 
 
 P = [v1a, v2a, v1b, v2b, eta1, eta2, a1, a2, pi1, pi2, Ter,sa];
--- models/code/criterionVar/Criterionx/fitmixture4x.m
+++ models/code/criterionVar/Criterionx/fitmixture4x.m
@@ -33,7 +33,7 @@ w = 2 * pi / nw;
 %h = tmax / 300; 
 
 % Set manually if needed - 30/1/19
-badix = 25; 
+badix = 5; 
 
 
 if nargin < 7
