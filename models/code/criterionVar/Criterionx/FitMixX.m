b752cb6aa2fa1f79bea27f90f6a8f35bd7871b4e
Jason Zhou <jasonz1@student.unimelb.edu.au>
2019-02-08 12:19:38 +1100

    Group Fits 3

--- models/code/criterionVar/Criterionx/FitMixX.m
+++ models/code/criterionVar/Criterionx/FitMixX.m
@@ -4,16 +4,16 @@ function [ll, bic, Pred, pest] = FitMixX(data)
 %          1    2    3    4    5      6    7   8   9   10    11
 setopt;
 
-v1a = normrnd(2,0.5);
+v1a = normrnd(6,0.5);
 v2a = normrnd(0.01,0.5);
-v1b = normrnd(2,0.2);
+v1b = normrnd(6,0.5);
 v2b = normrnd(0.01,0.5);
 eta1 = normrnd(0, 0.01);
 eta2 = normrnd(0,0.01);
 a1 = normrnd(2,0.5);
 a2 = normrnd(1.5,0.5);
-pi1 = norm(0.7,0.5);
-pi2 = normrnd(0.7,0.5);
+pi1 = norm(0.4,0.5);
+pi2 = normrnd(0.4,0.5);
 Ter = normrnd(0.15,0.05);
 sa = normrnd(1,0.5);
 
