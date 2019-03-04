2d66e8eefe5328f0bb76926db526f6078b3e403a
Jason Zhou <jasonz1@student.unimelb.edu.au>
2019-03-05 10:34:05 +1100

    JointDist
    Getting joint distribution out of my good plots

---
+++ models/code/gvm/UoMFitPrograms/fitGVM_fixed.m
@@ -0,0 +1,20 @@
+function [ll, bic, Pred, pest, Gstuff] = fitGVM_fixed(data, i)
+%    [ll,bic,Pred] = fitmixture3(Pvar, Pfix, Sel, Data)
+%    P = [v1a, v2a, v1b, v2b, eta1, eta2, a1, a2, pi1, pi2, Ter]
+%          1    2    3    4    5      6    7   8   9   10    11
+setopt;
+
+load("Good_StartingParam");
+%P=[nunorm1, nunorm2, kappa1, kappa2, eta, rho, a, Ter, st, sa];
+P = Starting_Preds{i,4};
+P(1,10) = 0; %TURN THIS OFF 
+Sel = [1,1,1,1,1,1,1,1,1,0];  % all parameters free
+%Sel = [0,0,0,0,0,0,0,0,0,0]; %all parameters fixed
+nlow = length(data{1,1});
+nhigh = length(data{1,2});
+
+pest = fminsearch(@fitgvm6x, P(Sel==1), options, P(Sel==0), Sel, data, nlow, nhigh);
+P(Sel==1) = pest;
+[ll,bic,Pred, Gstuff] = fitgvm6x(P(Sel==1), P(Sel==0), Sel, data, nlow, nhigh); %Criterion Range Error
+%fitplot(D11, Pred);
+end
---
+++ models/code/gvm/UoMFitPrograms/fitScript_fixed.m
@@ -0,0 +1,52 @@
+
+%% Open Data
+
+%Opens and organises the data into recognised/unrecognised and low/high imageability
+%Fit_Data = Treatment_Simple('stimulus.xlsx'); 
+%This is real slow, so I
+%have Jindiv_simple for my data set. For future use, switch this on, and
+%the following line off. You will need to do this in Treatment_Simple as
+%well.
+Fit_Data = Treatment_Simple;
+
+Recognised = Fit_Data(:,1);
+Unrecognised = Fit_Data(:,2);
+
+%Fit the data, generate predictions.
+
+participants = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20];
+%% Fit Variable Precision (Continuous model)
+
+% %Empty array for Log Likelihoods and Predictions to live.
+
+%Empty array for Log Likelihoods and Predictions to live.
+Fixed_Preds = cell(length(participants),5);
+for i = participants
+    i
+[ll, bic, Pred, pest, Gstuff] = fitGVM_fixed(Recognised {i}, i);
+Fixed_Preds{i,1} = ll;
+Fixed_Preds{i,2} = bic;
+Fixed_Preds{i,3} = Pred;
+Fixed_Preds{i,4} = pest;
+Fixed_Preds{i,5} = Gstuff;
+end
+
+save("gvmfit_crit2")
+% %% Plot Fits superimposed on Data, and save.
+% % Plot 
+% fitplot(Recognised {1}, Pred);
+% for i = participants
+%    
+%     filename = ['GVM_Recog',num2str(i),'.png'];
+%     fitplot(Recognised {i}, Fixed_Preds{i,3});
+%     saveas(gcf,filename);
+%     
+% end    
+
+
+%%
+%%% The thing I want at the end is...
+% full_data = open_data(filename)
+% data = split_data(full_data)
+% fit_data(data, starting_preds)
+% plot_data_preds(plot_filename, data, preds)
