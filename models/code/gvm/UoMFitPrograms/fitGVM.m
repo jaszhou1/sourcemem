10207dbfc1ade33850f3a838975055f6c4f42b42
Jason Zhou <jasonz1@student.unimelb.edu.au>
2019-03-04 11:30:45 +1100

    update FitGVM.m
    testing Phil param

--- models/code/gvm/UoMFitPrograms/fitGVM.m
+++ models/code/gvm/UoMFitPrograms/fitGVM.m
@@ -15,21 +15,12 @@ setopt;
 % st = normrnd(0.05,0.1);
 % sa = 0;
 %sa = normrnd(1,0.8);
+P = [2.48635633531635,2.50657983082597,0.329211490420301,0.788896541814084,1.02507162664465,0.105044258602163,2.11612703760500,-0.271300468914334,0.0785703065442259,0.182082253457239];
 
-nunorm1 = normrnd(2,1.5);
-nunorm2 = normrnd(2,1.5);
-kappa1 = normrnd(5,2);
-kappa2 = normrnd(5,2);
-eta = normrnd(0.5, 0.2);
-rho = normrnd(0.5,0.2);
-a = normrnd(3,0.5);
-Ter = normrnd(-0.2,0.3);
-st = normrnd(0.05,0.1);
-sa = 0;
 
-P=[nunorm1, nunorm2, kappa1, kappa2, eta, rho, a, Ter, st, sa];
+%P=[nunorm1, nunorm2, kappa1, kappa2, eta, rho, a, Ter, st, sa];
 
-Sel = [1,1,1,1,1,1,1,1,1,0];  % all parameters free
+Sel = [0,0,0,0,0,0,0,0,0,0];  % all parameters free
 nlow = length(data{1,1});
 nhigh = length(data{1,2});
 
