134d723a9b4de0687e209b4bd4e44cc735a807b0
Jason Zhou <jasonz1@student.unimelb.edu.au>
2019-03-27 18:07:09 +1100

    GVM_Crit
    Fits of the GVM model with criterion variability

--- models/code/criterionVar/Criterionx/FitHybridCrit.m
+++ models/code/criterionVar/Criterionx/FitHybridCrit.m
@@ -15,50 +15,50 @@ Unrecognised = Fit_Data(:,2);
 %Fit the data, generate predictions.
 
 participants = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20];
-%% Fit Variable Precision (Continuous model)
-
+% %% Fit Variable Precision (Continuous model)
+% 
+% % %Empty array for Log Likelihoods and Predictions to live.
+% VP_LL_Preds_Recognised = cell(length(participants),4);
+% for i = participants
+%     i
+% [ll, bic, Pred, pest] = FitVP(Recognised {i});
+% VP_LL_Preds_Recognised{i,1} = ll;
+% VP_LL_Preds_Recognised{i,2} = bic;
+% VP_LL_Preds_Recognised{i,3} = Pred;
+% VP_LL_Preds_Recognised{i,4} = pest;
+% end
+% 
+% VP_LL_Preds_Unrecognised = cell(length(participants),4);
+% for i = participants
+%     i
+% [ll, bic, Pred, pest] = FitVP(Unrecognised {i});
+% VP_LL_Preds_Unrecognised{i,1} = ll;
+% VP_LL_Preds_Unrecognised{i,2} = bic;
+% VP_LL_Preds_Unrecognised{i,3} = Pred;
+% VP_LL_Preds_Unrecognised{i,4} = pest;
+% end
+% %% Fit Mixture Model (Threshold model)
+% 
 % %Empty array for Log Likelihoods and Predictions to live.
-VP_LL_Preds_Recognised = cell(length(participants),4);
-for i = participants
-    i
-[ll, bic, Pred, pest] = FitVP(Recognised {i});
-VP_LL_Preds_Recognised{i,1} = ll;
-VP_LL_Preds_Recognised{i,2} = bic;
-VP_LL_Preds_Recognised{i,3} = Pred;
-VP_LL_Preds_Recognised{i,4} = pest;
-end
-
-VP_LL_Preds_Unrecognised = cell(length(participants),4);
-for i = participants
-    i
-[ll, bic, Pred, pest] = FitVP(Unrecognised {i});
-VP_LL_Preds_Unrecognised{i,1} = ll;
-VP_LL_Preds_Unrecognised{i,2} = bic;
-VP_LL_Preds_Unrecognised{i,3} = Pred;
-VP_LL_Preds_Unrecognised{i,4} = pest;
-end
-%% Fit Mixture Model (Threshold model)
-
-%Empty array for Log Likelihoods and Predictions to live.
-MX_LL_Preds_Recognised = cell(length(participants),4);
-for i = participants
-    i
-[ll, bic, Pred, pest] = FitMix(Recognised {i});
-MX_LL_Preds_Recognised{i,1} = ll;
-MX_LL_Preds_Recognised{i,2} = bic;
-MX_LL_Preds_Recognised{i,3} = Pred;
-MX_LL_Preds_Recognised{i,4} = pest;
-end
-
-MX_LL_Preds_Unrecognised = cell(length(participants),4);
-for i = participants
-    i
-[ll, bic, Pred, pest] = FitMix(Unrecognised {i});
-MX_LL_Preds_Unrecognised{i,1} = ll;
-MX_LL_Preds_Unrecognised{i,2} = bic;
-MX_LL_Preds_Unrecognised{i,3} = Pred;
-MX_LL_Preds_Unrecognised{i,4} = pest;
-end
+% MX_LL_Preds_Recognised = cell(length(participants),4);
+% for i = participants
+%     i
+% [ll, bic, Pred, pest] = FitMix(Recognised {i});
+% MX_LL_Preds_Recognised{i,1} = ll;
+% MX_LL_Preds_Recognised{i,2} = bic;
+% MX_LL_Preds_Recognised{i,3} = Pred;
+% MX_LL_Preds_Recognised{i,4} = pest;
+% end
+% 
+% MX_LL_Preds_Unrecognised = cell(length(participants),4);
+% for i = participants
+%     i
+% [ll, bic, Pred, pest] = FitMix(Unrecognised {i});
+% MX_LL_Preds_Unrecognised{i,1} = ll;
+% MX_LL_Preds_Unrecognised{i,2} = bic;
+% MX_LL_Preds_Unrecognised{i,3} = Pred;
+% MX_LL_Preds_Unrecognised{i,4} = pest;
+% end
 
 %% Fit VP + Mix Model
 
