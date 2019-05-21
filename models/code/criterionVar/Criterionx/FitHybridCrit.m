e368af8c6099cd0d9d7de00c661caac81ca7d391
Jason Zhou <jasonz1@student.unimelb.edu.au>
2019-05-21 17:59:06 +1000

    Prep for Fit 2

--- models/code/criterionVar/Criterionx/FitHybridCrit.m
+++ models/code/criterionVar/Criterionx/FitHybridCrit.m
@@ -22,7 +22,7 @@ participants = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20];
 % a badix value specific to the individual is passed down through to the
 % fitting procedure.
 
-badixs = [5,5,5,5,5,5,5,2,5,1,5,5,5,0,5,5,5,5,2,2];
+badixs = [25,5,15,10,5,5,5,10,5,1,5,5,3,0,5,5,5,5,5,5];
 
 % %% Fit Variable Precision (Continuous model)
 % [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20];
@@ -134,35 +134,35 @@ for i = participants
     end
 end
 
-save('UpdatedCritFits')
+save('Censored2')
 
 %% Plot Fits superimposed on Data, and save.
 % % Plot
-%for i = participants
+% for i = participants
 %    filename = ['Cont_Recog',num2str(i),'.png'];
 %    fitplot(Recognised {i}, VP_LL_Preds_Recognised{i,3});
 %    saveas(gcf,filename);
-
+% 
 %    filename = ['Cont_Unrecog',num2str(i),'.png'];
 %    fitplot(Unrecognised {i}, VP_LL_Preds_Unrecognised{i,3});
 %    saveas(gcf,filename);
-
+% 
 %    filename = ['Thresh_Recog',num2str(i),'.png'];
 %    fitplot(Recognised {i}, MX_LL_Preds_Recognised{i,3});
 %    saveas(gcf,filename);
-
+% 
 %    filename = ['Thresh_Unrecog',num2str(i),'.png'];
 %    fitplot(Unrecognised {i}, MX_LL_Preds_Unrecognised{i,3});
 %    saveas(gcf,filename);
-
+% 
 %    filename = ['Hybrid_Recog',num2str(i),'.png'];
 %    fitplot(Recognised {i}, HY_LL_Preds_Recognised{i,3});
 %    saveas(gcf,filename);
-
+% 
 %    filename = ['Hybrid_Unrecog',num2str(i),'.png'];
 %    fitplot(Unrecognised {i}, HY_LL_Preds_Unrecognised{i,3});
 %    saveas(gcf,filename);
-%end
+% end
 
 
 %%
