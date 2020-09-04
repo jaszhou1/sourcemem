5a9cb5d3748e13f1b1f497b29eaa22fea0360968
Jason Zhou <jasonz1@student.unimelb.edu.au>
2020-09-05 04:16:17 +1000

    Simulate datasets
    Added function that, at a top level, takes the best fits of each model to empirical data and uses those parameters to generate a simulated dataset from each of those models to be used in a model recovery exercise.

--- EXPIMG/models/code/simulation/dirichlet1.m
+++ EXPIMG/models/code/simulation/dirichlet1.m
@@ -31,7 +31,11 @@ szt = length(T);
 Gt = zeros(sztheta, szt);
 nonterminating = 0; 
 for i = 1:ntrials
-   [~,~,Ta,theta, nonterm] = dirisim(P, h, tmax);
+   [~,~,Ta,theta, nonterm] = dirisim(P, h, tmax); 
+   % This is actually simulating a run, so we don't need to draw samples in
+   % another function afterwards, scroll down to see dirisim.
+
+% The simulation is done in dirisim, what happens aftter
    thetaindex = min(find(theta <= Thetabound));
    if isempty(thetaindex)  % between -pi and Thetabound(1), pool into last
       thetaindex = 1;
@@ -42,17 +46,24 @@ for i = 1:ntrials
    tindex = min(max(find(Ta > tbound)),szt);  % Pool into last bin.
    if ~nonterm
        Nt(thetaindex) = Nt(thetaindex) + 1;
-       Mt(thetaindex) = Mt(thetaindex) + Ta;
+       Mt(thetaindex) = Mt(thetaindex) + Ta; 
    end;    
+   % Gt used to hold theta components
    Gt(thetaindex,tindex) = Gt(thetaindex, tindex) + 1;
 end;
 nonterminating = nonterminating + nonterm
 Mt = Mt./(Nt + eps);
+% Pt is the number of trials that have hit a bin, divided by the total
+% trials. A proportion of trials
+
 Pt = Nt / ntrials;
+
+% Simulation was oscilliatory, Philip's applied this filter to take a
+% window 
 filter = cos(-pi/2:.025:pi/2); % was .1 
 filter = filter / sum(filter);  % Normalize mass in filter
 for thetaindex = 1:sztheta
-    Gtfi = conv(Gt(thetaindex,:), filter);
+    Gtfi = conv(Gt(thetaindex,:), filter); 
     %Gt(i,:) = Gtfi(1:szt)./(Nt(i) * h +eps); % conditional
     Gt(thetaindex,:) = Gtfi(1:szt)./(ntrials  * h);  % joint density
 end
@@ -62,6 +73,8 @@ end;
 Commonscale = exp(-0.5 * (mu1^2/sigma1^2 + mu2^2/sigma2^2) * T);
 % Multiply theta-dependent drift term by invariant time-dependent term
 Mtscale = Pmt' * Commonscale;
+
+% Pt rescaled to be a density
 Pt = Pt / w; % To make into a density estimate.
 end
 
@@ -71,31 +84,48 @@ function [T,Xt, Ta,theta,nonterm] = dirisim(P, h, tmax)
 % drift from a circular region.
 % [T,Xt, Ta,theta,nonterm] = dirichlet1(P, h)
 % P = [mu1, mu2, sigma1, sigma2, a]
+
+% This is a direct simulation, prior to the correction for oscilliatory
+% behaviour- is this a problem? 
 % ======================================================================
-mu1 = P(1);
+mu1 = P(1); % This is the drift
 mu2 = P(2);
+
+% The diffusion coefficient scales everything else. The diffusion model
+% isn't identifiable if you estimate everything (boundary, drift and coefficent are all proportion)
+% Everything becomes relative to that coefficient
+% p. 432 of Smith 2016
 sigma1 = P(3);
 sigma2 = P(4);
 a = P(5);
 a2 = a * a;
 T = [0:h:tmax];
 nmax = length(T);
+
+% Xt is the sample path 
 Xt = zeros(2, nmax);
+% Sigma is diffusion coefficient (not drift variability) <- the diffusion
+% part
 Sigma_Wt = [sigma1 * randn(1,nmax); ...
             sigma2 * randn(1,nmax)]; 
+% mu1 and mu2 is drift in x and y co-ords
 Mut = [mu1 * ones(1,nmax); ...
        mu2 * ones(1, nmax)];
 Dt2 = 0;
 i = 2;
 a2 = a * a;
 while(Dt2 < a2 && i <= nmax)
+    % Each time step is the drift component (Xt bit) plus change over time
+    % And the diffusion (Sigma_wt) times the square root of time
     Xt(:,i) =  Xt(:,i-1) + Mut(:,i) * h + Sigma_Wt(:,i) * sqrt(h);
+    % Euclidean distance of currently sample path away from the centre
+    % point
     Dt2 = Xt(1, i)^2 + Xt(2,i)^2;
     i = i + 1;
 end;
 nonterm = i > nmax;
 % ???
-Ta = (i-1) * h;
+Ta = (i-1) * h; % Ta is time
 %Ta = (i-3) * h;
 theta = atan2(Xt(2, i-1), Xt(1,i-1));
        
---
+++ EXPIMG/models/code/simulation/simulate_data.m
@@ -0,0 +1,131 @@
+function [simdata_cont, simdata_thresh] = simulate_data()
+%% simulate_data
+% Generate sets of simulated data for each model using parameters that provided the best
+% fit for each model for each participant. Each simulated dataset should be
+% based on the same number of observations as the actual empirical
+% dataset. Then, fit the simulated data with each model to see if model
+% selection correctly identifies the model that generated that simulated
+% data.
+%% Load in the empirical data
+data = Treatment_Simple();
+% Participants
+participants = [1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20];
+
+% Determine the number of observations per participant
+nobvs = double.empty(20, 0);
+for i = participants
+    this_participant_nobvs = size(data{i,1}{1,1},1) + size(data{i,1}{1,2},1);
+    nobvs(i) = this_participant_nobvs;
+end
+
+%% Load in the best fitting parameters of each model to that data
+load('2020_02_10_19_28_pooled_badix_5.mat')
+
+% Arguments for the diffusion process simulation code
+tmax = 5.1;
+nt = 301;
+h = tmax / nt;
+nw = 50;
+d_angle = 2*pi/nw; %Angle space
+%% Simulate the continuous model
+simdata_cont = cell(20,1);
+for i = participants
+    % For this participant:
+    
+    % Parameters of best fit of the continuous model to this participant
+    %    P = [v1, v2, eta,  a, Ter, st, sa]
+    %          1   2    3    4   5   6   7
+    pest = VP_LL_Preds_Recognised{i,8}(1,:);
+    
+    % Get number of trials to simulate (same as empirical dataset for that
+    % participant)
+    ntrials = nobvs(i);
+    
+    % This is the data structure that will contain the simulated
+    % observations for this participant
+    this_sim = double.empty(0, 2);
+    for j = 1:ntrials
+        
+        % Arrange input vector that contains the  parameter values for simulation
+        
+        % Determine a drift rate by drawing from a gaussian with mean as the mean
+        % drift and with standard deviation eta (drift variability)
+        mu1 = max(0,normrnd(pest(1), pest(3)));
+        mu2 = pest(2);
+        % This is always equal to zero, since there was no evidence of systematic
+        % bias in response error in empirical data
+        sigma1 = 1; % This is the diffusion coefficient, and is always set to 1.
+        sigma2 = 1;
+        a = pest(4);
+        
+        
+        P = [mu1, mu2, sigma1, sigma2, a];
+        % Simulate a trial directly using dirisim
+        [T,Xt, Ta,theta,nonterm] = dirisim(P, h, tmax);
+        
+        % Store response error and response time of this trial
+        this_sim(j, 1) = theta;
+        this_sim(j, 2) = Ta;
+    end
+    simdata_cont{i,1} = this_sim;
+end
+
+%% Simulate the threshold model
+simdata_thresh = cell(20,1);
+for i = participants
+    % Parameters of best fit of the continuous model to this participant
+    %    P = [v1, v2, eta,   a1, a2, pi, Ter  st, sa]
+    %          1   2    3    4   5   6    7   8   9
+    pest = MX_LL_Preds_Recognised{i,8}(1,:);
+    
+    % Get number of trials to simulate (same as empirical dataset for that
+    % participant)
+    ntrials = nobvs(i);
+    
+    % This is the data structure that will contain the simulated
+    % observations for this participant
+    this_sim = double.empty(0, 2);
+    for j = 1:ntrials
+        
+        % Draw a random number and compare it to the mixing proportion (pi)
+        % to determine if we sample from a positive drift or a zero drift
+        % guessing process.
+        
+        if rand > pest(6) % Positive Drift
+            
+            % Arrange input vector that contains the  parameter values for simulation
+            mu1 = pest(1);
+            mu2 = pest(2);
+            sigma1 = 1;
+            sigma2 = 1;
+            a = pest(4);
+            
+            P = [mu1, mu2, sigma1, sigma2, a];
+            % Simulate a trial directly using dirisim
+            [T,Xt, Ta,theta,nonterm] = dirisim(P, h, tmax);
+            
+            % Store response error and response time of this trial
+            this_sim(j, 1) = theta;
+            this_sim(j, 2) = Ta;
+            
+        else % Zero Drift
+            
+            % Arrange input vector that contains the  parameter values for simulation
+            mu1 = 0;
+            mu2 = 0;
+            sigma1 = 1;
+            sigma2 = 1;
+            a = pest(5);
+            
+            P = [mu1, mu2, sigma1, sigma2, a];
+            % Simulate a trial directly using dirisim
+            [T,Xt, Ta,theta,nonterm] = dirisim(P, h, tmax);
+            
+            % Store response error and response time of this trial
+            this_sim(j, 1) = theta;
+            this_sim(j, 2) = Ta;
+        end
+    end
+    simdata_thresh{i,1} = this_sim;
+end
+end
