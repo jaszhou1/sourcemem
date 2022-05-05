6f0786b5fcd5f1b91d4aa82e870db561a1796e5c
Jason Zhou <jasonz1@student.unimelb.edu.au>
2022-05-05 12:59:16 +1000

    Construct new orthographic list
    Semantic list v4 is the most current semantic list. Rebuild orthographic lists excluding this batch of semantic words. all between 10 and 500 freq

--- EXPINT/experiment_stimuli/construct_orthographic_lists.R
+++ EXPINT/experiment_stimuli/construct_orthographic_lists.R
@@ -3,7 +3,7 @@ setwd("~/git/sourcemem/EXPINT/experiment_stimuli")
 # Load in semantic lists, exclude those words, construct orthographic
 # from the leftovers
 
-semantic.lists <- read.csv('semantic_lists_final_v2.csv')
+semantic.lists <- read.csv('semantic_list_filtered_v4.csv')
 
 # Load in a list of words and a JSON dictionary with word2vec vectors
 wordlist <- read.csv('subtlex_filtered_final.csv')
---
+++ EXPINT/experiment_stimuli/word_frequency_check.R
@@ -0,0 +1,24 @@
+# Check the word frequency of constructed lists
+
+setwd("~/git/sourcemem/EXPINT/experiment_stimuli")
+
+wordlist <- read.csv('subtlexLength.csv')
+wordlist$word <- tolower(wordlist$word)
+semantic <- read.csv('semantic_lists_final.csv')
+orthographic <- read.csv('orthographic_lists_final.csv')
+
+for(i in 1:nrow(semantic)){
+  this.list.length <- nrow(semantic[semantic$list == semantic$list[i],])
+  semantic$list_length[i] <- this.list.length
+  semantic$frequency[i] <- wordlist[wordlist$word == as.character(semantic$word[[i]]), 'FREQlow']
+}
+
+write.csv(semantic, file = 'semantic_list.csv')
+
+for(i in 1:nrow(orthographic)){
+  this.list.length <- nrow(orthographic[orthographic$list == orthographic$list[i],])
+  orthographic$list_length[i] <- this.list.length
+  orthographic$frequency[i] <- wordlist[wordlist$word == as.character(orthographic$word[[1]]), 'FREQlow']
+}
+
+write.csv(orthographic, file = 'orthographic_list.csv')
