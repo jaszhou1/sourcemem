1685117447dbe374f173f8a69e9692ebfbff2826
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2022-09-01 14:54:50 +1000

    Add orthosem model
    This is the orthographic/semantic model from last experiment. In it, chi, psi, and other parameters about semantics etc. are the same across conditions.

--- EXPINT/analysis/modelling/MATLAB/run_diffusion.m
+++ EXPINT/analysis/modelling/MATLAB/run_diffusion.m
@@ -5,7 +5,7 @@
 load('EXPINT_data.mat')
 
 n_participants = length(data);
-n_runs = 10;
+n_runs = 5;
 num_workers = maxNumCompThreads/2 - 1; % Maximum number of workers
 
 %% Saturated Model
@@ -56,4 +56,29 @@ parfor (i = 1:n_participants, num_workers)
     end
 end
 filename = [datestr(now,'yyyy_mm_dd_HH'),'_temp_saturated'];
+save(filename)
+
+%% Orthosem model with no difference across conditions
+orthosem = cell(n_participants,4);
+
+parfor (i = 1:n_participants, num_workers)
+    % Initial log likelihood value
+    ll = 1e7;
+    % Run each participant nrun times
+    this_fit = cell(1,4);
+    for j = 1:n_runs
+        this_participant_data = data(i,:);
+        [ll_new, aic, pest, pest_penalty] = fit_orthosem_model(this_participant_data);
+        % If this ll is better than the last one, replace it in the saved
+        % structure
+        if (ll_new < ll)
+            this_fit{1} = ll_new;
+            this_fit{2} = aic;
+            this_fit{3} = pest;
+            this_fit{4} = pest_penalty;
+            orthosem(i,:) = this_fit;
+        end
+    end
+end
+filename = [datestr(now,'yyyy_mm_dd_HH')];
 save(filename)
