a658319e3856790127eff2e832fad3ff948e64aa
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2022-09-19 17:23:14 +1000

    Plot response error simulation tests
    Add some functions to plot simulated recenters for the response error model code

--- EXPINT/analysis/modelling/R/model_code/compare_simulation_recenters.R
+++ EXPINT/analysis/modelling/R/model_code/compare_simulation_recenters.R
@@ -47,10 +47,74 @@ source('intrusion_gradient_model_conditions.R')
 saturated_P <- c(12, 10, 0.4, 0.2, 0.5, 0.7, 0.7,  0.5, 0.5, 0.1, 0.8, 1, 1, 2, 2, 0.3, 0.6)
 sim_saturated <- simulate_intrusion_cond_model(99, data, saturated_P)
 
+## Recntering
+source("~/git/sourcemem/EXPINT/analysis/plotting/response_error/resp_recenter_data.R")
+# Recenter data
+recenter_data <- recenter.data(data)
+
 # Recenter the spatiotemporal model
+recenter_spatiotemporal <- recenter.model(sim_spatiotemporal)
+
+# Recenter the orthosem model
+recenter_orthosem <- recenter.model(sim_orthosem)
 
 # recenter saturated model
+recenter_saturated <- recenter.model(sim_saturated)
 
 # Plot recentered spatiotemporal against data
+
+plot_temporal <- function(data, model, cond){
+  ggplot() + 
+    geom_histogram(data = data[data$cond == cond, ], aes(x = offset, y = ..density..), bins = 30) +
+    geom_histogram(data = model[model$cond == cond, ], aes(x = offset, y = ..density..), fill = 'red', alpha = 0.2, bins = 30) +
+    ylim(0, 0.5) + 
+    facet_grid(~lag) +
+    ggtitle(sprintf('%s Condition, Recentered on orthographic', cond))
+  
+}
+
+plot_spatial <- function(data, model, cond){
+  ggplot() + 
+    geom_histogram(data = data[data$cond == cond, ], aes(x = offset, y = ..density..), bins = 30) +
+    geom_histogram(data = model[model$cond == cond, ], aes(x = offset, y = ..density..), fill = 'red', alpha = 0.2, bins = 30) +
+    ylim(0, 0.5) + 
+    facet_grid(~spatial_bin) +
+    ggtitle(sprintf('%s Condition, Recentered on orthographic', cond))
+  
+}
+
+plot_orth <- function(data, model){
+  ggplot() + 
+    geom_histogram(data = data[data$cond == 'orthographic', ], aes(x = offset, y = ..density..), bins = 30) +
+    geom_histogram(data = model[model$cond == 'orthographic', ], aes(x = offset, y = ..density..), fill = 'red', alpha = 0.2, bins = 30) +
+    ylim(0, 0.5) + 
+    facet_grid(~orthographic) +
+    ggtitle(sprintf('%s Condition, Recentered on orthographic', 'orth'))
+  
+  ggplot() + 
+    geom_histogram(data = data[data$cond == 'unrelated', ], aes(x = offset, y = ..density..), bins = 30) +
+    geom_histogram(data = model[model$cond == 'unrelated', ], aes(x = offset, y = ..density..), fill = 'red', alpha = 0.2, bins = 30) +
+    ylim(0, 0.5) + 
+    facet_grid(~orthographic) +
+    ggtitle(sprintf('%s Condition, Recentered on orthographic', 'unrelated'))
+  
+  ggplot() + 
+    geom_histogram(data = data, aes(x = offset, y = ..density..), bins = 30) +
+    geom_histogram(data = model, aes(x = offset, y = ..density..), fill = 'red', alpha = 0.2, bins = 30) +
+    ylim(0, 0.5) + 
+    facet_grid(~orthographic) +
+    ggtitle(sprintf('%s Condition, Recentered on orthographic', 'overall'))
+  
+}
+
+plot_sem <- function(data, model, cond){
+  ggplot() + 
+    geom_histogram(data = data[data$cond == cond, ], aes(x = offset, y = ..density..), bins = 30) +
+    geom_histogram(data = model[model$cond == cond, ], aes(x = offset, y = ..density..), fill = 'red', alpha = 0.2, bins = 30) +
+    ylim(0, 0.5) + 
+    facet_grid(~semantic_bin) +
+    ggtitle(sprintf('%s Condition, Recentered on orthographic', cond))
+  
+}
 
               
