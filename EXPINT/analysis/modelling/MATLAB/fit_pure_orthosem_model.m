af22bd7a5c35bc5fe048a3ebcd3a6fe132625e18
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2022-10-06 17:04:45 +1100

    Add new models
    Modified the top level script to iterate through several reductions of the saturated model, save simulated data from each model once fitted. We want to see what parameters are important and which do not matter.

--- EXPINT/analysis/modelling/MATLAB/fit_pure_orthosem_model.m
+++ EXPINT/analysis/modelling/MATLAB/fit_pure_orthosem_model.m
@@ -1,7 +1,5 @@
-function [ll, aic, P, penalty] = fit_spatiotemporal_model(data, P, badix)
-% Same intrusion scaling and guessing across conditions, just change the
-% relative distribution of weight across the different kinds of similarity
-% when calculating intrusion likelihood
+function [ll, aic, P, penalty] = fit_pure_orthosem_model(data, P, badix)
+% Orthosem, no spatiotemporal part
 
 setopt;
 % Default value for badix (Cuts off the leading edge of RT)
@@ -45,18 +43,18 @@ if nargin < 3
     gamma3 = 0;
 
     % Temporal Gradient
-    tau = normrnd(0.6, 0.1); %Weight forwards vs backwards intrusion decay slope
-    l_b = normrnd(0.8, 0.1); % Decay of the backwards slope
-    l_f = normrnd(0.6, 0.1); % Decay of the forwards slope
-    zeta = normrnd(0.5, 0.1); %precision for Shepard similarity function (perceived spatial distance)
-    rho = normrnd(0.3, 0.1); % Spatial component weight in intrusion probability calculation
-    chi1 = 0; % Item v Context, Low
+    tau = 0; %Weight forwards vs backwards intrusion decay slope
+    l_b = 0; % Decay of the backwards slope
+    l_f = 0; % Decay of the forwards slope
+    zeta = 0; %precision for Shepard similarity function (perceived spatial distance)
+    rho = 0; % Spatial component weight in intrusion probability calculation
+    chi1 = 1; % Item v Context, Low
     chi2 = 0; % Item v Context, High
-    psi1 = 0; % Semantic v Ortho, Low
+    psi1 = normrnd(0.2, 0.1); % Semantic v Ortho, Low
     psi2 = 0; % Semantic v Ortho, High
-    iota1 = 0; % Ortho decay, Low
+    iota1 = normrnd(3, 0.1); % Ortho decay, Low
     iota2 = 0; % Ortho decay, High
-    upsilon1 = 0; % Semantic decay, Low
+    upsilon1 = normrnd(3, 0.1); % Semantic decay, Low
     upsilon2 = 0; % Semantic decay, High
     % Nondecision Time
     ter = normrnd(0.25, 0.05);
@@ -74,12 +72,12 @@ end
 %      1      2        3      4        5      6        7   8        9     10      11    12              13     14           15    16      17      18     19         20      21      22          23    24     25     26      27      28      29          30      31      32      33      34              35         36      37
 %   [v1t_1, v2t_1,  v1i_1,  v2i_1,  v1t_2, v2t_2,  v1i_2,  v2i_2, v1t_3, v2t_3,  v1i_3,  v2i_3,         eta_t, eta_i,       at,  ag,    beta1,  beta2,  beta3      gamma1, gamma2,  gamma3,     tau,  l_b,   l_f,   zeta,  rho,   chi1,     chi2,      psi1,   psi2,    iota1,  iota2,  upsilon1,   upsilon2,     Ter,    st]
 % ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Sel= [1,    0,      1,      0,     0,       0,      0,     0,     0,       0,      0,     0,           1,       1,          1,   1,        1,    1,     1,          1,      1,      1,           1,    1,    1,     1,      1,      0,      0,          0,      0,         0,    0,      0,          0,            1,     0];
+Sel= [1,    0,      1,      0,     0,       0,      0,     0,     0,       0,      0,     0,           1,       1,          1,   1,        1,    1,     1,          1,      1,      1,           0,    0,    0,     0,      0,      0,        0,      1,      0,         1,    0,      1,          0,            1,     0];
 
-Ub= [ 10,   1,      8,      1,     8,       1,      8,     1,     8,       1,      8,     1,           1,       1,          4.0, 3.0,     0.6,   0,      0,          0.6,    0,      0,          0.8,   2.5,  2.5,   0.7,   0.7,    0,      0,          0,      0,         0,     0,     0,          0,            0.5,    0.2];
-Lb= [ 1,    0,      0.2,    0,     0,       0,      0,     0,     0,       0,      0,     0,           0.01,    0.01,       0.1, 0.1,     0.05,  0,      0,          0.05,   0,      0,          0.2,   0.3,  0.3,   0.1,   0.1,    0,      0,          0,      0,         0,     0,     0,          0,            0.1,    0];
-Pub=[ 9,    0.9,    7,      0.9,   7,       0.9,    7,     0.9,   7,       0.9,    7,     0.9,         0.9,     0.9,        3.0, 2.5,     0.55,  0,      0,          0.55,   0,      0,          0.75,  2,    2,     0.6,   0.65,   0,      0,          0,      0,         0,     0,     0,          0,            0.45,   0.15];
-Plb=[ 1.1,  0,      0.5,    0,     0,       0,      0,     0,     0,       0,      0,     0,           0.05,    0.05,       0.7, 0.7,     0.1,   0,      0,          0.1,    0,      0,          0.25,  0.4,  0.4,   0.2,   0.2,    0,      0,          0,      0,         0,     0,     0,          0,            0.2,    0];
+Ub= [ 10,   1,      8,      1,     8,       1,      8,     1,     8,       1,      8,     1,           1,       1,          4.0, 3.0,     0.6,   0,      0,          0.6,    0,      0,          0,     0,    0,     0,     0,      1,      0,        0.7,      0,        6,     0,      6,          0,           0.5,    0.2];
+Lb= [ 1,    0,      0.2,    0,     0,       0,      0,     0,     0,       0,      0,     0,           0.01,    0.01,       0.1, 0.1,     0.05,  0,      0,          0.05,   0,      0,          0,     0,    0,     0,     0,      1,      0,        0,        0,        0.1,   0,    0.1,          0,           0.1,    0];
+Pub=[ 9,    0.9,    7,      0.9,   7,       0.9,    7,     0.9,   7,       0.9,    7,     0.9,         0.9,     0.9,        3.0, 2.5,     0.55,  0,      0,          0.55,   0,      0,          0,     0,    0,     0,     0,      1,      0,        0.6,      0,        5,     0,      5,          0,           0.45,   0.15];
+Plb=[ 1.1,  0,      0.5,    0,     0,       0,      0,     0,     0,       0,      0,     0,           0.05,    0.05,       0.7, 0.7,     0.1,   0,      0,          0.1,    0,      0,          0,     0,    0,     0,     0,      1,      0,        0.05,     0,        0.5,   0,    0.5,          0,           0.2,    0];
 
 Pbounds = [Ub; Lb; Pub; Plb];
 
--- EXPINT/analysis/modelling/MATLAB/run_full_diffusion.m
+++ EXPINT/analysis/modelling/MATLAB/run_full_diffusion.m
@@ -64,6 +64,7 @@ parfor (i = 1:n_participants, num_workers)
         end
     end
 end
+save(filename)
 
 %% Gamma Model
 gamma = cell(n_participants,4);
@@ -91,10 +92,13 @@ parfor (i = 1:n_participants, num_workers)
         end
     end
 end
+save(filename)
 
-%% Gamma Model
+%% Mix Cond Model
 % Neither gamma nor beta (nor drift) vary across conditions, just the
 % weight of different kinds of similarity for intrusions
+
+% The MIXture depends on the CONdition, but that's all.
 mix_cond = cell(n_participants,4);
 
 parfor (i = 1:n_participants, num_workers)
@@ -120,7 +124,102 @@ parfor (i = 1:n_participants, num_workers)
         end
     end
 end
+save(filename)
 
+%%%%%%%%%%%%%%%%%%%% Models not to be taken seriously, but check that
+%%%%%%%%%%%%%%%%%%%% certain properties of the full model work in isolation
+
+%% Spatiotemporal-Orthosem model
+% Orthography and Semantics do matter, but they don't differ across
+% conditions
+spatiotemporal_orthosem = cell(n_participants,4);
+
+parfor (i = 1:n_participants, num_workers)
+    % Initial log likelihood value, very large. 
+    ll = 1e7;
+    % Run each participant nrun times
+    this_fit = cell(1,4);
+    for j = 1:n_runs
+        this_participant_data = data(i,:);
+        [ll_new, aic, pest, pest_penalty] = fit_spatiotemporal_orthosem_model(this_participant_data);
+        % If this ll is better than the last one, replace it in the saved
+        % structure
+        if (ll_new < ll)
+            % Double fit, call fminsearch again, but use the best parameter
+            % estimates as starting points
+            [ll_new, aic, pest, pest_penalty] = fit_spatiotemporal_orthosem_model(this_participant_data, pest);
+            ll = ll_new;
+            this_fit{1} = ll;
+            this_fit{2} = aic;
+            this_fit{3} = pest;
+            this_fit{4} = pest_penalty;
+            spatiotemporal_orthosem(i,:) = this_fit;
+        end
+    end
+end
+save(filename)
+
+%% Spatiotemporal Model
+% No orthographic/semantic contribution in model, no difference between
+% conditions
+spatiotemporal = cell(n_participants,4);
+
+parfor (i = 1:n_participants, num_workers)
+    % Initial log likelihood value, very large. 
+    ll = 1e7;
+    % Run each participant nrun times
+    this_fit = cell(1,4);
+    for j = 1:n_runs
+        this_participant_data = data(i,:);
+        [ll_new, aic, pest, pest_penalty] = fit_spatiotemporal_model(this_participant_data);
+        % If this ll is better than the last one, replace it in the saved
+        % structure
+        if (ll_new < ll)
+            % Double fit, call fminsearch again, but use the best parameter
+            % estimates as starting points
+            [ll_new, aic, pest, pest_penalty] = fit_spatiotemporal_model(this_participant_data, pest);
+            ll = ll_new;
+            this_fit{1} = ll;
+            this_fit{2} = aic;
+            this_fit{3} = pest;
+            this_fit{4} = pest_penalty;
+            spatiotemporal(i,:) = this_fit;
+        end
+    end
+end
+save(filename)
+
+%% Pure Orthosem model
+% Orthography and Semantics do matter, but they don't differ across
+% conditions, and there is no spatiotemporal part to the model
+pure_orthosem = cell(n_participants,4);
+
+parfor (i = 1:n_participants, num_workers)
+    % Initial log likelihood value, very large. 
+    ll = 1e7;
+    % Run each participant nrun times
+    this_fit = cell(1,4);
+    for j = 1:n_runs
+        this_participant_data = data(i,:);
+        [ll_new, aic, pest, pest_penalty] = fit_pure_orthosem_model(this_participant_data);
+        % If this ll is better than the last one, replace it in the saved
+        % structure
+        if (ll_new < ll)
+            % Double fit, call fminsearch again, but use the best parameter
+            % estimates as starting points
+            [ll_new, aic, pest, pest_penalty] = fit_pure_orthosem_model(this_participant_data, pest);
+            ll = ll_new;
+            this_fit{1} = ll;
+            this_fit{2} = aic;
+            this_fit{3} = pest;
+            this_fit{4} = pest_penalty;
+            pure_orthosem(i,:) = this_fit;
+        end
+    end
+end
+save(filename)
+
+%% Simulation of the fitted models.
 
  sim_saturated = [];
  for i = 1:10
@@ -132,3 +231,63 @@ end
 header_line = 'participant, model_name, AIC, v1t_1, v2t_1,  v1i_1,  v2i_1,  v1t_2, v2t_2,  v1i_2,  v2i_2, v1t_3, v2t_3,  v1i_3,  v2i_3,         eta_t, eta_i,       at,  ag,    beta1,  beta2,  beta3,      gamma1, gamma2,  gamma3,     tau,  l_b,   l_f,   zeta,  rho,   chi1,     chi2,      psi1,   psi2,    iota1,  iota2,  upsilon1,   upsilon2,     Ter,    st';
 filename = [datestr(now,'yyyy_mm_dd_HH'),'_pest_saturated.csv'];
 param_to_csv(filename, 1:n_participants, saturated, 'Saturated', header_line);
+
+ sim_gammabeta = [];
+ for i = 1:10
+     this_sim = simulate_full_model(data(i,:), gammabeta{i,3}, i);
+     sim_gammabeta = vertcat(sim_gammabeta, this_sim);
+ end
+ csvwrite('sim_gammabeta.csv', sim_gammabeta)
+
+filename = [datestr(now,'yyyy_mm_dd_HH'),'_pest_gammabeta.csv'];
+param_to_csv(filename, 1:n_participants, gammabeta, 'gammabeta', header_line);
+
+ sim_gamma = [];
+ for i = 1:10
+     this_sim = simulate_full_model(data(i,:), gamma{i,3}, i);
+     sim_gamma = vertcat(sim_gamma, this_sim);
+ end
+ csvwrite('sim_gamma.csv', sim_gamma)
+ 
+filename = [datestr(now,'yyyy_mm_dd_HH'),'_pest_gamma.csv'];
+param_to_csv(filename, 1:n_participants, gamma, 'gamma', header_line);
+
+ sim_mix_cond = [];
+ for i = 1:10
+     this_sim = simulate_full_model(data(i,:), mix_cond{i,3}, i);
+     sim_mix_cond = vertcat(sim_mix_cond, this_sim);
+ end
+ csvwrite('sim_mix_cond.csv', sim_mix_cond)
+ 
+filename = [datestr(now,'yyyy_mm_dd_HH'),'_pest_mix_cond.csv'];
+param_to_csv(filename, 1:n_participants, mix_cond, 'mix_cond', header_line);
+
+ sim_spatiotemporal_orthosem = [];
+ for i = 1:10
+     this_sim = simulate_full_model(data(i,:), spatiotemporal_orthosem{i,3}, i);
+     sim_spatiotemporal_orthosem = vertcat(sim_spatiotemporal_orthosem, this_sim);
+ end
+ csvwrite('sim_spatiotemporal_orthosem.csv', sim_spatiotemporal_orthosem)
+ 
+filename = [datestr(now,'yyyy_mm_dd_HH'),'_pest_spatiotemporal_orthosem.csv'];
+param_to_csv(filename, 1:n_participants, spatiotemporal_orthosem, 'spatiotemporal_orthosem', header_line);
+
+ sim_spatiotemporal = [];
+ for i = 1:10
+     this_sim = simulate_full_model(data(i,:), spatiotemporal{i,3}, i);
+     sim_spatiotemporal = vertcat(sim_spatiotemporal, this_sim);
+ end
+ csvwrite('sim_spatiotemporal.csv', sim_spatiotemporal)
+ 
+filename = [datestr(now,'yyyy_mm_dd_HH'),'_pest_spatiotemporal.csv'];
+param_to_csv(filename, 1:n_participants, spatiotemporal, 'spatiotemporal', header_line);
+
+ sim_pure_orthosem = [];
+ for i = 1:10
+     this_sim = simulate_full_model(data(i,:), pure_orthosem{i,3}, i);
+     sim_pure_orthosem = vertcat(sim_pure_orthosem, this_sim);
+ end
+ csvwrite('sim_pure_orthosem.csv', sim_pure_orthosem)
+ 
+filename = [datestr(now,'yyyy_mm_dd_HH'),'_pest_pure_orthosem.csv'];
+param_to_csv(filename, 1:n_participants, pure_orthosem, 'pure_orthosem', header_line);
