96bf3127e882c9a536f92c9ce14817ae57b19af7
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2022-10-13 10:54:50 +1100

    Create list_similarities.R
    Creates a plot of all the pairwise comparisons between target and nontarget items in the dataset

---
+++ EXPINT/analysis/list_similarities.R
@@ -0,0 +1,53 @@
+# Illustrate the similarity difference of the lists that participants saw
+library(ggplot2)
+library(data.table)
+library(ggpubr)
+
+## Looking at data prior to modelling
+data <- read.csv("~/git/sourcemem/EXPINT/data/EXPINT_data.csv")
+data <- na.omit(data)
+
+data[data$condition == 'semantic', 'condition'] <- 'Semantic'
+data[data$condition == 'orthographic', 'condition'] <- 'Orthographic'
+data[data$condition == 'unrelated', 'condition'] <- 'Unrelated'
+
+
+
+orth.data <- melt(setDT(data), id.vars = c('participant', 'session', 'block', 'condition'), measure.vars = c('ortho1', 'ortho2', 'ortho3', 'ortho4', 'ortho5', 'ortho6', 'ortho7'))
+sem.data <- melt(setDT(data), id.vars = c('participant', 'session', 'block', 'condition'), measure.vars = c('semantic1', 'semantic2', 'semantic3', 'semantic4', 'semantic5', 'semantic6', 'semantic7'))
+
+sem.data <- sem.data[!((sem.data$condition == 'Semantic') & (sem.data$value < 0.3)),]
+
+orthographic.similarities <- ggplot(data = orth.data) +
+  geom_bar(aes(x = value, fill = condition)) +
+  facet_wrap(~condition, nrow = 1) +
+  scale_x_continuous(name = 'Orthographic (Damerau-Levenshtein) Distance', 
+                   breaks = 1:6, 
+                   labels = c(1,2,3,4,5,6)) +
+  scale_y_continuous(name = 'Frequency', 
+                   breaks = c(0, 6000, 12000), 
+                   labels = c(0, 6000, 12000)) +
+  theme(legend.position = 'none',
+        panel.background =  element_rect(fill = "white"),
+        panel.border = element_rect(linetype = "solid", fill = NA),
+        strip.background = element_rect(colour = "black", fill = "white"),
+        text=element_text(size=24))
+
+sem.similarities <- ggplot(data = sem.data) +
+  geom_histogram(aes(x = value, fill = condition), bins = 30) +
+  facet_wrap(~condition, nrow = 1) +
+  scale_x_continuous(name = 'Semantic (word2vec Cosine) Similarity',
+                     breaks = c(0, 0.25, 0.5, 0.75, 1),
+                     labels = c(0, 0.25, 0.5, 0.75, 1)) +
+  scale_y_continuous(name = 'Frequency', 
+                     breaks = c(0, 4000), 
+                     labels = c(0, 4000)) +
+  theme(legend.position = 'none',
+        panel.background =  element_rect(fill = "white"),
+        panel.border = element_rect(linetype = "solid", fill = NA),
+        strip.background = element_rect(colour = "black", fill = "white"),
+        text=element_text(size=24))
+
+setwd("~/git/sourcemem/EXPINT/analysis/plotting/output")
+plot <- ggarrange(orthographic.similarities, NULL, sem.similarities, ncol = 1, heights = c(1, 0.1, 1))
+ggsave('list_comparison.png', plot = last_plot(), width = 40, height = 20, units = "cm")
