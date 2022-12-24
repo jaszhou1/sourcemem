f039979efc89f2d42d1d5cabe28efd6aa9318f1d
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2022-12-24 23:53:56 +1100

    change some models back to upper
    The first two model fitting functions do use upper instead of a Pbounds matrix, so just change those two back

--- EXPINT/analysis/modelling/R/model_code/11_12_flat_gamma.R
+++ EXPINT/analysis/modelling/R/model_code/11_12_flat_gamma.R
@@ -67,7 +67,7 @@ flat_gamma1 <- function(data){
   this_fit <- DEoptim(intrusion_cond_model, lower, upper, control = DEoptim.control(itermax = 500), data, Pfix, Sel)
   
   # Calculate aic
-  aic <- get_aic(this_fit$optim$bestval, length(Pbounds[1,]))
+  aic <- get_aic(this_fit$optim$bestval, length(upper))
   this_fit$optim$aic<-aic
   
   # Assemble estimated parameter vector
