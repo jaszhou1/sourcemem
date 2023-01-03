c2952ed7420bc7b8c3421f32b1ed61f7a36038ea
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2023-01-03 11:39:14 +1100

    Fix indexing of Pest vector
    Forgot to update this line so it was calling 35 parameters rather than the needed 38 (including the phis, temporal weights).

--- EXPINT/analysis/modelling/R/model_code/run_fits_v2.R
+++ EXPINT/analysis/modelling/R/model_code/run_fits_v2.R
@@ -117,7 +117,7 @@ for(i in 1:length(models)){
   this_model <- model_fits[[i]]
   for(j in participants){
     this_participant_data <- data[data$participant == j,]
-    this_Pest <- this_model[j, 4:38]
+    this_Pest <- this_model[j, 4:41]
     this_sim_data <- simulate_intrusion_cond_model_x(j, this_participant_data, this_Pest, model_names[i])
     this_recentered_data <- recenter.model(this_sim_data, model_names[i])
     sim_data <- rbind(sim_data, this_sim_data)
