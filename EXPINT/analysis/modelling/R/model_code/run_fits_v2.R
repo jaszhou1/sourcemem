e45ca6023a9a3b1c1833039b0a71eff708e41131
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2023-01-13 11:19:51 +1100

    Update run_fits_v2.R
    I've attempted to parallelise the simulation part of the code (I don't know if there's an easy way to vectorise this stuff), seeing if it actually runs on linux

--- EXPINT/analysis/modelling/R/model_code/run_fits_v2.R
+++ EXPINT/analysis/modelling/R/model_code/run_fits_v2.R
@@ -33,7 +33,7 @@ source("~/git/sourcemem/EXPINT/analysis/modelling/R/model_code/intrusion_cond_mo
 participants <- unique(data$participant)
 conds <- unique(data$condition)
 
-# Function to calculate aic 
+# Function to calculate aic
 get_aic <- function(L, n_params){
   aic <- 2*L + 2*n_params
   return(aic)
@@ -72,7 +72,7 @@ source('x16_temporal_decay.R')
 source('x17_spatial_decay.R')
 source('x18_spatiotemporal_decay.R')
 
-models <- c(flat_intrusion, flat_gamma1, flat_gamma2, temporal, 
+models <- c(flat_intrusion, flat_gamma1, flat_gamma2, temporal,
             asym_temporal, spatial, ortho, temporal_ortho,
             spatial_ortho, spatiotemporal, spatiotemporal_ortho,
             four_factor, temporal_ortho_weight, spatial_ortho_weight,
@@ -83,7 +83,7 @@ model_names <- c('flat_intrusion', 'flat_gamma1', 'flat_gamma2', 'temporal',
                  'asym_temporal', 'spatial', 'ortho', 'temporal_ortho',
                  'spatial_ortho', 'spatiotemporal', 'spatiotemporal_ortho',
                  'four_factor', 'temporal_weight', 'spatial_weight',
-                 'spatiotemporal_weight', 'temporal_decay', 'spatial_decay', 
+                 'spatiotemporal_weight', 'temporal_decay', 'spatial_decay',
                  'spatiotemporal_decay')
 
 fit_model <- function(data, model, model_name){
@@ -98,61 +98,48 @@ fit_model <- function(data, model, model_name){
                    return(this_fit)
                  }
   res <- as.data.frame(res)
-  colnames(res) <- c('participant','nLL','aic', 'kappa1', 'kappa2', 'beta1', 'beta2', 'beta3', 'gamma1', 'gamma2', 'gamma3', 
+  colnames(res) <- c('participant','nLL','aic', 'kappa1', 'kappa2', 'beta1', 'beta2', 'beta3', 'gamma1', 'gamma2', 'gamma3',
                      'phi1', 'phi2', 'phi3', 'rho1', 'rho2', 'rho3', 'chi1', 'chi2', 'chi3', 'psi1', 'psi2', 'psi3', 'tau1', 'tau2', 'tau3', 'lambda_b1', 'lambda_f1', 'lambda_b2', 'lambda_f2', 'lambda_b3', 'lambda_f3','zeta1', 'zeta2', 'zeta3', 'iota1', 'iota2', 'iota3', 'upsilon1', 'upsilon2', 'upsilon3')
   write.csv(res, paste(toString(Sys.Date()), '_', model_name,'.csv', sep =""))
   return(res)
 }
 
-model_fits <- list()
-for(i in 1:length(models)){
-  this_model_fit <- fit_model(data, models[[i]], model_names[[i]])
-  model_fits <- append(model_fits, list(this_model_fit))
-}
-names(model_fits) <- model_names
-save(model_fits, file = paste(toString(Sys.Date()), '_fits.RData', sep =""))
-
-# Put all the aics together
-AICs <- setNames(data.frame(matrix(nrow = 10, ncol = length(model_fits))), model_names)
-for(i in 1:length(model_fits)){
-  AICs[,i] <- model_fits[[i]]$aic 
-}
-AICs[11,] <- colSums(AICs)
-source("~/git/sourcemem/EXPINT/analysis/modelling/R/model_code/AIC_weight.R") 
-wAIC <- AIC_weight(AICs, 'wAIC.csv')
-# 
-# # Simulate model predictions from the estimated parameters (big job because recentering)
-# source("~/git/sourcemem/EXPINT/analysis/modelling/R/model_code/resp_recenter_data.R")
-# sim_data <- data.frame()
-# recentered_sim_data <- data.frame()
+# model_fits <- list() # Can this be vectorised? Should be first port of call, before parallel looping
 # for(i in 1:length(models)){
-#   this_model <- model_fits[[i]]
-#   for(j in participants){
-#     this_participant_data <- data[data$participant == j,]
-#     this_Pest <- this_model[j, 4:41]
-#     this_sim_data <- simulate_intrusion_cond_model_x(j, this_participant_data, this_Pest, model_names[i])
-#     this_recentered_data <- recenter.model(this_sim_data, model_names[i])
-#     sim_data <- rbind(sim_data, this_sim_data)
-#     recentered_sim_data <- rbind(recentered_sim_data, this_recentered_data)
-#   }
+#   this_model_fit <- fit_model(data, models[[i]], model_names[[i]])
+#   model_fits <- append(model_fits, list(this_model_fit))
 # }
+# names(model_fits) <- model_names
+# save(model_fits, file = paste(toString(Sys.Date()), '_fits.RData', sep =""))
 # 
-# sim_data1 <- sim_data[sim_data$model %in% c('flat_intrusion', 'flat_gamma1', 'flat_gamma2', 'temporal',
-#                                             'asym_temporal', 'spatial', 'ortho', 'temporal_ortho',
-#                                             'spatial_ortho', 'spatiotemporal'), ]
-# sim_data2 <- sim_data[!(sim_data$model %in% c('flat_intrusion', 'flat_gamma1', 'flat_gamma2', 'temporal',
-#                                             'asym_temporal', 'spatial', 'ortho', 'temporal_ortho',
-#                                             'spatial_ortho', 'spatiotemporal')), ]
-# 
-# recentered_data <- recenter.data(data)
-# 
-# # Bundle everything up and save output
-# # save(recentered_data, file = paste(toString(Sys.Date()), '_simulated.RData', sep =""))
-# save(sim_data1, file = paste(toString(Sys.Date()), '_simulated1.RData', sep =""))
-# save(sim_data2, file = paste(toString(Sys.Date()), '_simulated2.RData', sep =""))
-# save(recentered_sim_data, file = paste(toString(Sys.Date()), '_simulated_recenter.RData', sep =""))
+# # Put all the aics together
+# AICs <- setNames(data.frame(matrix(nrow = 10, ncol = length(model_fits))), model_names)
+# for(i in 1:length(model_fits)){
+#   AICs[,i] <- model_fits[[i]]$aic
+# }
+# AICs[11,] <- colSums(AICs)
+# source("~/git/sourcemem/EXPINT/analysis/modelling/R/model_code/AIC_weight.R")
+# wAIC <- AIC_weight(AICs, 'wAIC.csv')
 
-
-
+# Simulate model predictions from the estimated parameters (big job because recentering)
+source("~/git/sourcemem/EXPINT/analysis/modelling/R/model_code/resp_recenter_data.R")
+model_simulations <- list()
+for(i in 1:length(models)){
+  this_model <- model_fits[[i]]
+  this_model_name <- model_names[i]
+  
+  cl <- makeForkCluster((detectCores() - 1))
+  registerDoParallel(cl)
+  
+  res = foreach (j = 1:length(participants)) %dopar% {
+                   this_participant_data <- data[data$participant == j,]
+                   this_Pest <- this_model[j, 4:41]
+                   this_sim_data <- simulate_intrusion_cond_model_x(j, this_participant_data, this_Pest, this_model_name)
+                   this_recentered_data <- recenter.model(this_sim_data, this_model_name)
+                   return(list(this_sim_data, this_recentered_data))
+                 }
+  model_simulations <- append(model_simulations, res)
+}
+save(model_simulations, file = paste(toString(Sys.Date()), '_Preds.RData', sep =""))
 
 
